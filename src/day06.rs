3501a0ddb1e5128a44ffdcd20cb9ffd0f1bbfc22
Oliver Newman <orn688@gmail.com>
2021-12-06 20:19:03 -0500

    Finish day 6, part 2
    The original implementation for part 1 turned out to be super
    inefficient, since it iterated over every single fish on every day. Now
    we just keep track of the number of fish there are for each number of
    days before spawning. The runtime is now linear in terms of the number
    of days, as opposed to exponential, and uses effectively constant space.

--- src/day06.rs
+++ src/day06.rs
@@ -1,37 +1,48 @@
-const TOTAL_DAYS: i32 = 80;
+use std::collections::HashMap;
+
 const VALUE_AFTER_BIRTH: i32 = 6;
 const NEW_FISH_VALUE: i32 = 8;
 
 pub fn part1(input: &str) -> Result<String, String> {
-    let mut fish = vec![];
-    parse_input(input, &mut fish);
-    for _ in 0..TOTAL_DAYS {
-        let mut new_fish = vec![];
-        for f in fish {
-            if f == 0 {
-                new_fish.push(VALUE_AFTER_BIRTH);
-                new_fish.push(NEW_FISH_VALUE);
+    Ok(population_after_days(input, 80).to_string())
+}
+
+pub fn part2(input: &str) -> Result<String, String> {
+    Ok(population_after_days(input, 256).to_string())
+}
+
+fn population_after_days(input: &str, days: i32) -> usize {
+    let fish = parse_input(input);
+    let mut counts: HashMap<i32, usize> = HashMap::new();
+    for f in fish.into_iter() {
+        incr_count(&mut counts, f, 1);
+    }
+    for _ in 0..days {
+        let mut new_counts = HashMap::new();
+        for (f, count) in counts.into_iter() {
+            if f > 0 {
+                incr_count(&mut new_counts, f - 1, count);
             } else {
-                new_fish.push(f - 1);
+                incr_count(&mut new_counts, NEW_FISH_VALUE, count);
+                incr_count(&mut new_counts, VALUE_AFTER_BIRTH, count);
             }
         }
-        fish = new_fish;
+        counts = new_counts;
     }
-    Ok(fish.len().to_string())
+    counts.values().sum()
 }
 
-pub fn part2(_: &str) -> Result<String, String> {
-    Ok(String::new())
+fn incr_count(counter: &mut HashMap<i32, usize>, k: i32, amount: usize) {
+    let val = counter.entry(k).or_insert(0);
+    *val += amount;
 }
 
-fn parse_input(input: &str, fish: &mut Vec<i32>) {
-    fish.extend(
-        input
-            .trim()
-            .split(',')
-            .map(|s| s.parse::<i32>().unwrap())
-            .into_iter(),
-    );
+fn parse_input(input: &str) -> Vec<i32> {
+    input
+        .trim()
+        .split(',')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect()
 }
 
 #[allow(dead_code)]
@@ -44,5 +55,5 @@ fn test_part1() {
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2(TEST_INPUT).unwrap(), "");
+    assert_eq!(part2(TEST_INPUT).unwrap(), "26984457539");
 }
