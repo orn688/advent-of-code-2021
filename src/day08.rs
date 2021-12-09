8929b8ef475c502aeace1ab6d1bc25c02abeb4e0
Oliver Newman <orn688@gmail.com>
2021-12-08 19:04:18 -0500

    Finish day 8, part 2
    Super convoluted code, but it works surpisingly well.

--- src/day08.rs
+++ src/day08.rs
@@ -1,8 +1,50 @@
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
 
 lazy_static! {
     // Length of each digit that's represented by a unique number of segments.
     static ref UNIQUE_DIGIT_LENGTHS: Vec<usize> = vec![2, 3, 4, 7];
+    static ref DIGIT_SEGMENTS: HashMap<&'static str, i32> = {
+        let mut map = HashMap::new();
+        map.insert("abcefg", 0);
+        map.insert("cf", 1);
+        map.insert("acdeg", 2);
+        map.insert("acdfg", 3);
+        map.insert("bcdf", 4);
+        map.insert("abdfg", 5);
+        map.insert("abdefg", 6);
+        map.insert("acf", 7);
+        map.insert("abcdefg", 8);
+        map.insert("abcdfg", 9);
+        map
+    };
+    #[derive(Debug)]
+    static ref SEGMENT_LENGTH_SIGNATURES: HashMap<String, char> = {
+        let digits = DIGIT_SEGMENTS.keys().copied().collect();
+        segment_digit_lengths(digits)
+    };
+}
+
+fn segment_digit_lengths(digits: Vec<&str>) -> HashMap<String, char> {
+    let mut map: HashMap<char, HashMap<usize, usize>> = HashMap::new();
+    for digit in digits {
+        for c in digit.chars() {
+            let counts = map.entry(c).or_default();
+            let count = counts.entry(digit.len()).or_default();
+            *count += 1;
+        }
+    }
+    let mut inverted_map = HashMap::new();
+    for (char, lengths) in map.iter() {
+        let mut key_parts = lengths
+            .iter()
+            .map(|(length, count)| format!("{}:{}", length, count))
+            .collect::<Vec<String>>();
+        key_parts.sort();
+        inverted_map.insert(key_parts.join(","), *char);
+    }
+    inverted_map
 }
 
 pub fn part1(input: &str) -> Result<String, String> {
@@ -18,8 +60,38 @@ pub fn part1(input: &str) -> Result<String, String> {
     Ok(count.to_string())
 }
 
-pub fn part2(_: &str) -> Result<String, String> {
-    Ok(String::new())
+/// Takes advantange of the fact that for every line segment that makes up a part
+/// of a digit, that line segment has a unique "signature". The signature is
+/// defined by a counter that, for each number of per-digit segments, maps to the
+/// number of digits that have that number of segments.
+pub fn part2(input: &str) -> Result<String, String> {
+    let lines = input.trim().lines();
+    let mut sum = 0;
+    for line in lines {
+        let halves: Vec<_> = line.split(" | ").collect();
+        let patterns: Vec<_> = halves[0].split_whitespace().collect();
+        let mut translation: HashMap<char, char> = HashMap::new();
+
+        for (signature, c) in segment_digit_lengths(patterns) {
+            translation.insert(c, *SEGMENT_LENGTH_SIGNATURES.get(&signature).unwrap());
+        }
+
+        let mut line_sum = 0;
+        for val in halves[1].split_whitespace() {
+            let mut translated = val
+                .chars()
+                .map(|c| *translation.get(&c).unwrap())
+                .collect::<Vec<char>>();
+            translated.sort_unstable();
+            let val = *DIGIT_SEGMENTS
+                .get(translated.iter().collect::<String>().as_str())
+                .unwrap();
+            line_sum *= 10;
+            line_sum += val;
+        }
+        sum += line_sum;
+    }
+    Ok(sum.to_string())
 }
 
 #[allow(dead_code)]
@@ -43,5 +115,5 @@ fn test_part1() {
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2(TEST_INPUT).unwrap(), "");
+    assert_eq!(part2(TEST_INPUT).unwrap(), "61229");
 }
