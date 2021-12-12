e3e4deda193a766b7d3de8c88a1bb5e6eb9970be
Oliver Newman <orn688@gmail.com>
2021-12-12 15:37:32 -0500

    Finish day 12, part 2
    Pretty similar to part 1 but with significant enough differences that I
    decided to just copy-paste.
    
    Not super efficient, the solutions for day 12 take ~130ms total compared
    to ~60ms total for all the previous days. Maybe I'll come back later and
    optimize them a bit.

--- src/day12.rs
+++ src/day12.rs
@@ -4,15 +4,20 @@ use anyhow::Result;
 
 pub fn part1(input: &str) -> Result<String> {
     let graph = parse_input(input);
-    let count = count_paths(&graph, "start", "end", &mut HashSet::new());
+    let count = count_paths_part1(&graph, "start", "end", &mut HashSet::new());
     Ok(count.to_string())
 }
 
-fn count_paths(graph: &Graph, curr: &str, target: &str, visited: &mut HashSet<String>) -> usize {
+fn count_paths_part1(
+    graph: &Graph,
+    curr: &str,
+    target: &str,
+    visited: &mut HashSet<String>,
+) -> usize {
     if curr.eq(target) {
         return 1;
     }
-    if visited.contains(curr) && !can_revisit(curr) {
+    if visited.contains(curr) && !can_revisit_part1(curr) {
         return 0;
     }
 
@@ -21,7 +26,7 @@ fn count_paths(graph: &Graph, curr: &str, target: &str, visited: &mut HashSet<St
     let count = graph
         .neighbors(curr)
         .iter()
-        .map(|n| count_paths(graph, n, target, visited))
+        .map(|n| count_paths_part1(graph, n, target, visited))
         .sum();
 
     if newly_visited {
@@ -31,8 +36,63 @@ fn count_paths(graph: &Graph, curr: &str, target: &str, visited: &mut HashSet<St
     count
 }
 
-pub fn part2(_: &str) -> Result<String> {
-    Ok(String::new())
+/// A node can be revisited if its name is all-caps.
+fn can_revisit_part1(node: &str) -> bool {
+    is_uppercase(node)
+}
+
+pub fn part2(input: &str) -> Result<String> {
+    let graph = parse_input(input);
+    let count = count_paths_part2(&graph, "start", "end", &mut HashSet::new(), false);
+    Ok(count.to_string())
+}
+
+fn count_paths_part2(
+    graph: &Graph,
+    curr: &str,
+    target: &str,
+    visited: &mut HashSet<String>,
+    // Whether one lowercase node has already been revisited.
+    mut revisited_one: bool,
+) -> usize {
+    if curr.eq(target) {
+        return 1;
+    }
+    if visited.contains(curr) {
+        if !can_revisit_part2(curr, revisited_one) {
+            return 0;
+        } else if !is_uppercase(curr) {
+            revisited_one = true;
+        }
+    }
+
+    let newly_visited = visited.insert(curr.to_string());
+
+    let count = graph
+        .neighbors(curr)
+        .iter()
+        .map(|n| count_paths_part2(graph, n, target, visited, revisited_one))
+        .sum();
+
+    if newly_visited {
+        visited.remove(curr);
+    }
+
+    count
+}
+
+/// A node can be revisited if its name is all-caps. Likewise, it cannot be
+/// revisited if its name is all lowercase, and each node can only be on or the
+/// other, so it's sufficient to check if it has *any* uppercase letter.
+fn can_revisit_part2(node: &str, revisited_one: bool) -> bool {
+    if node == "start" || node == "end" {
+        return false;
+    }
+    !revisited_one || is_uppercase(node)
+}
+
+fn is_uppercase(s: &str) -> bool {
+    s.chars().all(|c| c.is_uppercase())
 }
 
 fn parse_input(input: &str) -> Graph {
@@ -44,13 +104,6 @@ fn parse_input(input: &str) -> Graph {
         graph.add_edge(ends[0].to_string(), ends[1].to_string());
     }
     graph
-}
-
-/// A node can be revisited if its name is all-caps. Likewise, it cannot be
-/// revisited if its name is all lowercase, and each node can only be on or the
-/// other, so it's sufficient to check if it has *any* uppercase letter.
-fn can_revisit(node: &str) -> bool {
-    node.chars().any(|c| c.is_uppercase())
 }
 
 struct Graph {
@@ -99,5 +152,5 @@ fn test_part1() {
 
 #[test]
 fn test_part2() {
-    assert_eq!(part2(TEST_INPUT).unwrap(), "");
+    assert_eq!(part2(TEST_INPUT).unwrap(), "3509");
 }
